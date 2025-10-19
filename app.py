1c5542d9f9698b1114557cefec5addfa4f243438
juanccm717-cyber <juanccm717@gmail.com>
2025-10-19 10:52:28 -0500

    app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 # --- Configuración de Supabase ---
 # Es mejor usar variables de entorno, pero para simplificar, las ponemos aquí.
-SUPABASE_URL = "https://ylzpvpgcelbsbdcauqzw.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsenB2cGdjZWxic2JkY2F1cXp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3OTkzNzcsImV4cCI6MjA3NjM3NTM3N30.b4v7l5Js-Dju1DCJ4oTnP5e2wzvFpM0qHTS8PtCY7Fc"
-
+SUPABASE_URL = os.environ.get("SUPABASE_URL" )
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 # Crear el cliente de Supabase
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY )
 
@@ -54,3 +53,4 @@ def search_diagnosticos():
 # Esta sección permite ejecutar el servidor con `python app.py` y activa el modo debug
 if __name__ == '__main__':
     app.run(debug=True)
+
